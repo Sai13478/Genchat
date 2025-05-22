27d5d9de8bfc4057742c3463eaa4fdcc402a3f1c
SAIPRAMODU VEDANTAM <saipramodvedantam@gmail.com>
2025-05-22 17:20:53 +0530

    uri change

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://saipramodvedantam:cFsFJ37viwgHdMec@cluster0.rtwqnkw.mongodb.net/rc_a?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
