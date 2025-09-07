ba34a9c1988a43f2cfab4f9fc5544819108a81a2
SAIPRAMODU VEDANTAM <saipramodvedantam@gmail.com>
2025-09-07 23:23:26 +0530

    .csx issue mc

--- frontend/src/lib/apiClient.js
+++ frontend/src/lib/apiClient.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
 	// Construct the base URL by appending '/api' to the VITE_API_URL
+	
 	baseURL: `${import.meta.env.VITE_API_URL}/api`,
 	withCredentials: true, // Important for sending cookies
 	headers: { "Content-Type": "application/json" },
