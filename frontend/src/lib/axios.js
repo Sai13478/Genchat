92b811ae4969cf8eb788c8409b32193560adf9be
SAIPRAMODU VEDANTAM <saipramodvedantam@gmail.com>
2025-09-08 15:54:14 +0530

    /api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://genchat-vi93.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
