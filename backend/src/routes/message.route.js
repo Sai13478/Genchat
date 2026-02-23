69e8fc7fd1c23173b68f061cf8bab48992cfe5d6
Sai13478 <saipramodvedantam@gmail.com>
2026-02-23 21:55:23 +0530

    router post fix 1

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -16,10 +16,10 @@ const router = express.Router();
 // This route MUST be protected to know who the current user is
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/search", protectRoute, searchUser);
-router.post("/friend-request/:id", protectRoute, sendFriendRequest);
 router.get("/friend-requests", protectRoute, getFriendRequests);
 router.post("/friend-request/accept", protectRoute, acceptFriendRequest);
 router.post("/friend-request/reject", protectRoute, rejectFriendRequest);
+router.post("/friend-request/:id", protectRoute, sendFriendRequest);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
