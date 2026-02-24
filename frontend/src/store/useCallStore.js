0771bbb6e9bba286122f43ae075f7bd6c3c595ac
Sai13478 <saipramodvedantam@gmail.com>
2026-02-24 15:08:57 +0530

    added turn servers to bypass fix 9

--- frontend/src/store/useCallStore.js
+++ frontend/src/store/useCallStore.js
@@ -8,17 +8,23 @@ const configuration = {
     iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
         {
             urls: [
                 "turn:openrelay.metered.ca:80",
                 "turn:openrelay.metered.ca:443",
-                "turn:openrelay.metered.ca:443?transport=tcp"
+                "turn:openrelay.metered.ca:443?transport=tcp",
+                "turns:openrelay.metered.ca:443?transport=tcp"
             ],
             username: "openrelayproject",
             credential: "openrelayproject",
         },
     ],
     iceCandidatePoolSize: 10,
+    bundlePolicy: "max-bundle",
+    rtcpMuxPolicy: "require",
 };
 
 export const useCallStore = create((set, get) => ({
